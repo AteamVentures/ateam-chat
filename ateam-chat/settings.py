f774a7bbd954a4efe3b87fd840e4f10073d905db
libbom14 <libbom14@gmail.com>
2018-08-13 08:43:14 +0900

    [API] add paginations

--- ateam-chat/settings.py
+++ ateam-chat/settings.py
@@ -66,6 +66,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination',
 }
 
 TEMPLATES = [
